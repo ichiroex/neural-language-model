9e49ebab45b49fbe9c481d45704a0e6d83acc8e5
Soichiro Murakami <plan.sk8.b@gmail.com>
2016-07-04 19:11:37 +0900

    ネットワークを修正

--- net.py
+++ net.py
@@ -18,11 +18,13 @@ class NLM(Chain):
 
         super(NLM, self).__init__(
             embed = L.EmbedID(vocab_size, embed_size),
-            l1 = L.Linear(embed_size, hidden_size),
+            l1 = L.Linear(embed_size*2, hidden_size),
             l2 = L.Linear(hidden_size, vocab_size))
 
-    def __call__(self, x):
-        h0 = self.embed(x)
-        h1 = F.tanh(self.l1(h0))
+    def __call__(self, x1, x2):
+        e1 = self.embed(x1)
+        e2 = self.embed(x2)
+        e = F.concat((e1, e2))
+        h1 = F.tanh(self.l1(e))
         y = self.l2(h1)
         return y
