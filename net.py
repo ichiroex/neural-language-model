320fcc8f1455d1b2a995906e4e99369c4642769b
Soichiro Murakami <plan.sk8.b@gmail.com>
2016-07-05 22:52:56 +0900

    文脈窓サイズを指定

--- net.py
+++ net.py
@@ -14,16 +14,18 @@ class NLM(Chain):
     def __init__(self,
                  vocab_size,
                  embed_size,
-                 hidden_size):
+                 hidden_size,
+                 context_window):
 
         super(NLM, self).__init__(
             embed = L.EmbedID(vocab_size, embed_size),
-            l1 = L.Linear(embed_size*2, hidden_size),
+            l1 = L.Linear(embed_size * (context_window-1), hidden_size),
             l2 = L.Linear(hidden_size, vocab_size))
 
         self.vocab_size = vocab_size
         self.embed_size = embed_size
         self.hidden_size = hidden_size
+        self.context_window = context_window
 
     def __call__(self, x1, x2):
         e1 = self.embed(x1)
@@ -42,7 +44,7 @@ class NLM(Chain):
             print(self.vocab_size, file=fp)
             print(self.embed_size, file=fp)
             print(self.hidden_size, file=fp)
-
+            print(self.context_window, file=fp)
 
     @staticmethod
     def load_spec(filename):
@@ -51,4 +53,5 @@ class NLM(Chain):
             vocab_size = int(next(fp))
             embed_size = int(next(fp))
             hidden_size = int(next(fp))
-            return NLM(vocab_size, embed_size, hidden_size)
+            context_window = int(next(fp))
+            return NLM(vocab_size, embed_size, hidden_size, context_window)
